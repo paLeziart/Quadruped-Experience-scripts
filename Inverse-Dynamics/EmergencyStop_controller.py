2322b66e0afc5160493928131e9b188899d382b6
Pierre-Alexandre Leziart <paleziart@laas.fr>
2020-01-31 13:20:41 +0100

    [PA] Starting to work with TSID for the 12dof

--- Inverse-Dynamics/EmergencyStop_controller.py
+++ Inverse-Dynamics/EmergencyStop_controller.py
@@ -8,7 +8,7 @@
 ########################################################################
 
 
-import numpy as np 
+import numpy as np
 
 
 ########################################################################
@@ -16,18 +16,34 @@ import numpy as np
 ########################################################################
 
 class controller:
-	
-	def __init__(self, qdes, vdes):
-		self.error = False
-		self.qdes = qdes
-		self.vdes = vdes
-		
-	####################################################################
-	#                      Torque Control method                       #
-	####################################################################
-	def control(self, qmes, vmes):
-		
-		# Torque controller
-		tau = np.zeros((8,1))
-		
-		return tau.flatten()
+
+    def __init__(self, qdes, vdes):
+        self.error = False
+        self.qdes = qdes
+        self.vdes = vdes
+
+    ####################################################################
+    #                      Torque Control method                       #
+    ####################################################################
+    def control(self, qmes, vmes):
+
+        # Torque controller
+        tau = np.zeros((8, 1))
+
+        return tau.flatten()
+
+
+class controller_12dof:
+
+    def __init__(self):
+        self.error = False
+
+    ####################################################################
+    #                      Torque Control method                       #
+    ####################################################################
+    def control(self, qmes12, vmes12):
+
+        # Torque controller
+        tau = np.zeros((12, 1))
+
+        return tau.flatten()
--- Inverse-Dynamics/main_simu_12dof.py
+++ Inverse-Dynamics/main_simu_12dof.py
@@ -15,9 +15,9 @@ import EmergencyStop_controller
 ########################################################################
 #                        Parameters definition                         #
 ########################################################################
-	
+
 # Simulation parameters
-N_SIMULATION = 10000	# number of time steps simulated
+N_SIMULATION = 10000  # number of time steps simulated
 
 t = 0.0  				# time
 
@@ -31,32 +31,35 @@ t_list = []
 ########################################################################
 
 # Start the client for PyBullet
-physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 
 # Load horizontal plane
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
 
 # Set the gravity
-p.setGravity(0,0,-9.81)
+p.setGravity(0, 0, -9.81)
 
 # Load Quadruped robot
-robotStartPos = [0,0,0.5] 
-robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
+robotStartPos = [0, 0, 0.5]
+robotStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 p.setAdditionalSearchPath("/opt/openrobots/share/example-robot-data/robots/solo_description/robots")
-robotId = p.loadURDF("solo.urdf",robotStartPos, robotStartOrientation)
+robotId = p.loadURDF("solo12.urdf", robotStartPos, robotStartOrientation)
 
 # Disable default motor control for revolute joints
-revoluteJointIndices = [0,1, 3,4, 6,7, 9,10]
-p.setJointMotorControlArray(robotId, jointIndices = revoluteJointIndices, controlMode = p.VELOCITY_CONTROL,targetVelocities = [0.0 for m in revoluteJointIndices], forces = [0.0 for m in revoluteJointIndices])
+revoluteJointIndices = [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14]
+p.setJointMotorControlArray(robotId, jointIndices=revoluteJointIndices, controlMode=p.VELOCITY_CONTROL, targetVelocities=[
+                            0.0 for m in revoluteJointIndices], forces=[0.0 for m in revoluteJointIndices])
 
 # Initialize the robot in a specific configuration
-#p.resetJointStatesMultiDof(robotId, revoluteJointIndices, q0)						
+#p.resetJointStatesMultiDof(robotId, revoluteJointIndices, q0)
 
 # Enable torque control for revolute joints
 jointTorques = [0.0 for m in revoluteJointIndices]
-
 p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+
+# Fix the base in the world frame
+#p.createConstraint(robotId, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0.5])
 
 # Set time step for the simulation
 p.setTimeStep(dt)
@@ -68,64 +71,67 @@ p.setTimeStep(dt)
 
 myController = controller(q0, omega, t)
 mySafetyController = Safety_controller.controller_12dof()
-myEmergencyStop = EmergencyStop_controller.controller()
+myEmergencyStop = EmergencyStop_controller.controller_12dof()
 
-for i in range (N_SIMULATION):
-	
-	time_start = time.time()
-		
-	####################################################################
-	#                 Data collection from PyBullet                    #
-	####################################################################
-	
-	jointStates = p.getJointStates(robotId, revoluteJointIndices) # State of all joints
-	baseState   = p.getBasePositionAndOrientation(robotId)
-	
-	# Joints configuration and velocity vector
-	qmes8 = np.vstack((np.array([baseState[0]]).T, np.array([baseState[1]]).T, np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
-	vmes8 = np.vstack((np.zeros((6,1)), np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
-	
-	# Conversion (from 8 to 12 DOF) for TSID computation 
-	qmes12 = np.concatenate((qmes8[:7], np.matrix([0.]), qmes8[7:9], np.matrix([0.]), qmes8[9:11], np.matrix([0.]), qmes8[11:13], np.matrix([0.]), qmes8[13:15]))
-	vmes12 = np.concatenate((vmes8[:6], np.matrix([0.]), vmes8[6:8], np.matrix([0.]), vmes8[8:10], np.matrix([0.]), vmes8[10:12], np.matrix([0.]), vmes8[12:14]))
-	
-	####################################################################
-	#                Select the appropriate controller 				   #
-	#								&								   #
-	#				Load the joint torques into the robot			   #
-	####################################################################
-	
-	# If the limit bounds are reached, controller is switched to a pure derivative controller
-	if(myController.error):
-		print("Safety bounds reached. Switch to a safety controller")
-		myController = mySafetyController
-		
-	# If the simulation time is too long, controller is switched to a zero torques controller
-	time_error = time_error or (time.time()-time_start > 0.001)
-	if (time_error):
-		print("Computation time lasted to long. Switch to a zero torque control")
-		myController = myEmergencyStop
-		
-	# Retrieve the joint torques from the appropriate controller
-	jointTorques = myController.control(qmes12, vmes12, t)
-		
-	# Set control torque for all joints
-	p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
-	
-	# Compute one step of simulation
-	p.stepSimulation()
-	
-	# Time incrementation
-	t += dt
-	
-	time_spent = time.time() - time_start
-	
-	t_list.append(time_spent)
+for i in range(N_SIMULATION):
 
-## Plot the tracking of the trajectories	
+    time_start = time.time()
+
+    ####################################################################
+    #                 Data collection from PyBullet                    #
+    ####################################################################
+
+    jointStates = p.getJointStates(robotId, revoluteJointIndices)  # State of all joints
+    baseState = p.getBasePositionAndOrientation(robotId)
+
+    # Joints configuration and velocity vector
+    qmes8 = np.vstack((np.array([baseState[0]]).T, np.array([baseState[1]]).T, np.array(
+        [[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
+    vmes8 = np.vstack((np.zeros((6, 1)), np.array(
+        [[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
+
+    # Conversion (from 8 to 12 DOF) for TSID computation
+    qmes12 = np.concatenate((qmes8[:7], np.matrix([0.]), qmes8[7:9], np.matrix(
+        [0.]), qmes8[9:11], np.matrix([0.]), qmes8[11:13], np.matrix([0.]), qmes8[13:15]))
+    vmes12 = np.concatenate((vmes8[:6], np.matrix([0.]), vmes8[6:8], np.matrix(
+        [0.]), vmes8[8:10], np.matrix([0.]), vmes8[10:12], np.matrix([0.]), vmes8[12:14]))
+
+    ####################################################################
+    #                Select the appropriate controller 				   #
+    #								&								   #
+    #				Load the joint torques into the robot			   #
+    ####################################################################
+
+    # If the limit bounds are reached, controller is switched to a pure derivative controller
+    if(myController.error):
+        print("Safety bounds reached. Switch to a safety controller")
+        myController = mySafetyController
+
+    # If the simulation time is too long, controller is switched to a zero torques controller
+    time_error = time_error or (time.time()-time_start > 0.001)
+    if (time_error):
+        print("Computation time lasted to long. Switch to a zero torque control")
+        myController = myEmergencyStop
+
+    # Retrieve the joint torques from the appropriate controller
+    jointTorques = np.zeros((12, 1))  # myController.control(qmes12, vmes12, t)  #  
+
+    # Set control torque for all joints
+    p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+
+    # Compute one step of simulation
+    p.stepSimulation()
+
+    # Time incrementation
+    t += dt
+
+    time_spent = time.time() - time_start
+
+    t_list.append(time_spent)
+
+# Plot the tracking of the trajectories
 
 plt.figure(1)
 plt.plot(t_list, 'k+')
 
 plt.show()
-
