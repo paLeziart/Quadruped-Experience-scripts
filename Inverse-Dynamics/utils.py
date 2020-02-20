be932f4eea118dc9c752aebd8d333bf3baa37f88
paLeziart <paleziart@laas.fr>
2020-02-20 17:37:51 +0100

    Creating utils.py to store short utility functions

---
+++ Inverse-Dynamics/utils.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+
+##########################
+# ROTATION MATRIX TO RPY #
+##########################
+
+# Taken from https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+
+# Checks if a matrix is a valid rotation matrix.
+
+
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R):
+
+    assert(isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
+
+###########################################################
+#  Roll Pitch Yaw (3 x 1) to Quaternion function (4 x 1)  #
+###########################################################
+
+
+def getQuaternion(rpy):
+    c = np.cos(rpy*0.5)
+    s = np.sin(rpy*0.5)
+    cy = c[2, 0]
+    sy = s[2, 0]
+    cp = c[1, 0]
+    sp = s[1, 0]
+    cr = c[0, 0]
+    sr = s[0, 0]
+
+    w = cy * cp * cr + sy * sp * sr
+    x = cy * cp * sr - sy * sp * cr
+    y = sy * cp * sr + cy * sp * cr
+    z = sy * cp * cr - cy * sp * sr
+
+    return np.array([[x, y, z, w]]).transpose()
